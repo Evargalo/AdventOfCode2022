598cac0d4f4d18ac8e19796e9b4ded2566ab9290
Olivier Pucher <olivier.pucher@insee.fr>
2022-11-30 21:37:33 +0100

    Add files via upload

---
+++ day1.R
@@ -0,0 +1,11 @@
+setwd("C:/Users/mmajr1/Documents/Perso/AdventOfCode/AoC_2022")
+day1A <- read_table("day8A.txt", col_names = FALSE)
+day1A <- read_delim("day8A.txt", col_names = FALSE, delim=' ')
+longtest <- read_delim("day1test.txt", col_names = FALSE, delim=' ')
+
+# A
+
+
+
+# B
+
