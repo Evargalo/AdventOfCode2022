f2fb21f86536200d6c2f9254179ac7531e23c2a6
Pucher Olivier <MMAJR1@ad.insee.intra>
2022-12-08 07:21:21 +0100

    day8

--- day8.R
+++ day8.R
@@ -8,9 +8,10 @@ dat<-df_from_vector_of_strings(d$X1) %>% mutate_all(as.numeric) %>% as.matrix
 size<-dat %>% ncol 
 
 # A ----
-res<-dat
 
-res[,]<-1
+res<-dat
+res[,]<-1 # arbre visible sauf si caché dans les 4 directions
+
 for(i in 2:(size-1)){
   for(j in 2:(size-1)){
     if(any(dat[1:(i-1),j]>=dat[i,j]) &
@@ -27,39 +28,44 @@ sum(res)
 
 
 # B ----
-res[,]<-1
-# i<-4;j<-3
+
+res[,]<-1 # scenic score
+
 for(i in 2:(size-1)){
   for(j in 2:(size-1)){
-    val<-dat[i,j]
+    val<-dat[i,j] # hauteur de l'arbre
+    
     if(all(dat[1:(i-1),j]<val)){
-      res[i,j]<-res[i,j]*(i-1)
+      k<-(i-1)
     } else{
       w<-which(dat[(i-1):1,j]>=val)
       k<-first(w)
-      res[i,j]<-res[i,j]*k
     } 
+    res[i,j]<-res[i,j]*k
+    
     if(all(dat[size:(i+1),j]<val)){
-      res[i,j]<-res[i,j]*(size-i)
+      k<-(size-i)
     } else{
       w<-which(dat[(i+1):size,j]>=val)
       k<-first(w)
-      res[i,j]<-res[i,j]*k
     } 
+    res[i,j]<-res[i,j]*k
+    
     if(all(dat[i,1:(j-1)]<val)){
-      res[i,j]<-res[i,j]*(j-1)
+      k<-(j-1)
     } else{
       w<-which(dat[i,(j-1):1]>=val)
       k<-first(w)
-      res[i,j]<-res[i,j]*k
     } 
+    res[i,j]<-res[i,j]*k
+    
     if(all(dat[i,size:(j+1)]<val)){
-      res[i,j]<-res[i,j]*(size-j)
+      k<-(size-j)
     } else{
       w<-which(dat[i,(j+1):size]>=val)
       k<-first(w)
-      res[i,j]<-res[i,j]*k
     } 
+    res[i,j]<-res[i,j]*k
   }
 }
 max(res)
