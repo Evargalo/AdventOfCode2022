31769f0a2b2c865bef5bf2c2d29e3e398661b1f4
Pucher Olivier <MMAJR1@ad.insee.intra>
2022-12-15 15:07:36 +0100

    day 15

--- programs/day15.R
+++ programs/day15.R
@@ -1,55 +1,49 @@
 # Donnees ----
 
-d <- read_delim("data/data15test.txt",col_names = FALSE, delim='=')
+# d <- read_delim("data/data15test.txt",col_names = FALSE, delim='=')
 d <- read_delim("data/data15.txt",col_names = FALSE, delim='=')
 
 d %<>% select(-X1) %>% 
   mutate(X2=cutAfter(X2,','),X4=cutAfter(X4,','),X3=cutAfter(X3,':')) %>% 
-  mutate_all(as.numeric)
+  mutate_all(as.numeric) # X2,X3 : sensor's coordinates / X4,X5 : beacon's coordinates
 
 minX<-min(c(d$X2,d$X4))-max(abs(d$X2-d$X4))
 maxX<-max(c(d$X2,d$X4))+max(abs(d$X2-d$X4))
 minY<-min(c(d$X3,d$X5))-max(abs(d$X3-d$X5))
 maxY<-max(c(d$X3,d$X5))+max(abs(d$X3-d$X5))
+
 manDist<-function(a,b,c,d)  abs(c-a)+abs(d-b)
-d %<>% mutate(mD=manDist(X2,X3,X4,X5))
+
+d %<>% mutate(mD=manDist(X2,X3,X4,X5)) # manDist between sensor and beacon
 
 # A ----
-yR<-10 
+# yR<-10 
 yR<-2000000
 
-d %>% filter(abs(X3-yR)<=mD) %>% 
-  mutate(fr=X2-mD+abs(X3-yR),to=X2+mD-abs(X3-yR)) ->dd
+d %>% filter(abs(X3-yR)<=mD) %>% # if a sensor is too far it doesn't interfere
+  mutate(fr=X2-mD+abs(X3-yR),to=X2+mD-abs(X3-yR) # X-values in the interval [fr;to] are too close from the sensor
+         ) ->dd
 
-dd %<>% mutate(mDfr=manDist(X2,X3,fr,yR))
+imposs<-d %>% filter(X5==yR) %>% pull(X4) %>% unique # impossible values for Y: initialized where there is already a beacon 
+length(imposs)->beacOnLine # how many beacons on this row ?
 
-imposs<-d %>% filter(X5==yR) %>% pull(X4) %>% unique 
-length(imposs)->beacOnLine
-
-addImposs<-function(fr,to){
-  imposs<<-c(fr:to,imposs) %>% unique
+addImposs<-function(fr,to){ # aggregate impossible intervals
+  imposs<<-c(fr:to,imposs) %>% unique 
 }
-pmap(dd %>% select(fr,to),addImposs)
+suppressMessages(pmap(dd %>% select(fr,to),addImposs))
 
-length(imposs) - beacOnLine
+length(imposs) - beacOnLine # nb of impossible spots
 # 5176944
 
 
 # B ----
 
-# x,y in 0: 4000000
-bSup<-20
+# x,y in 0:4000000
+
+#bSup<-20
 bSup<-4000000
 
 # Fastest strategy: by collapsing intervals ----
-findHoles<-function(df){ # given a df of intervals, find any non-included value
-  df %<>% arrange(fr,to) %>% mutate(gap=fr-lag(to))
-  df %>% filter(gap>1) %>% pull(fr)-1
-}
-# Test
-df<-data.frame(fr=c(0,25,36),to=c(28,34,bSup))
-df<-data.frame(fr=c(0,25,36),to=c(28,35,bSup))
-findHoles(df)
 
 X<-0
 while(X < bSup){ # given X, find any possible Y
@@ -61,32 +55,31 @@ while(X < bSup){ # given X, find any possible Y
   d1 %>% filter(mD>=dX) %>% # Only for sensors not too far away, otherwise they don't interfere
     rowwise() %>% mutate(fr=max(X3-mD+dX,0),to=min(X3+mD-dX,bSup)) %>% # Y can't be in [fr;to]
     ungroup->dd
+
   # remove any interval inside another one
-  # dd %>% rowwise %>% 
-  #   mutate(inside=sum((dd$fr<=fr & dd$to>to) | (dd$fr<fr & dd$to>=to))) %>% # We don't want to remove duplicated intervals
-  #   ungroup %>% filter(inside<1) -> ddd
-  
-  dd %>% select(fr,to) %>% unique %>% rowwise %>% 
-    mutate(inside=sum(dd$fr<=fr & dd$to>=to)) %>% ungroup %>% 
+  dd %>% select(fr,to) %>% unique %>% 
+    rowwise %>% mutate(inside=sum(dd$fr<=fr & dd$to>=to)) %>% ungroup %>% 
     filter(inside==1) -> ddd
   
   # Does any Y feat ?
-  # ddd %<>% arrange(fr,to) %>% mutate(gap=fr-lag(to))
-  # l<- ddd %>% filter(gap>1) %>% pull(fr)-1
-  
-  l<-findHoles(ddd %>% select(fr,to)) 
+  ddd %<>% arrange(fr,to) %>% # sort intervals
+    mutate(gap=fr-lag(to)) # gap between intervals. A negative gap means that the intervals overlaps with the previous one 
+  l<- ddd %>% filter(gap>1) %>% 
+    pull(fr)-1 # If any gap is above 1, the value in between is the possible Y
   if(length(l)>0) {print("gagné!");break}
   
+  # Otherwise we want to try another column (i.e. increase X, but not just by 1 because that's too slow)
   # how far can we jump forward ? Let's see by comparing intervals
-  dd %>% select(dX,fr,to,dir) %>% mutate(one=1) -> ddd
-  ddd %>% full_join(ddd,by=c("one")) %>% 
-    filter(to.x!=to.y | fr.y!=fr.x)%>% 
-    filter(!(to.x+1<=fr.y | to.y+1<=fr.x)) %>% 
-    rowwise %>% 
-    mutate(dir=dir.x*dir.y,overlap=min(to.x-fr.y,to.y-fr.x)) %>%
-    ungroup  %>% filter(dir!=-1) %>% 
-    pull(overlap) ->ovl
-  move<-min(d1$dX,ovl)%/%2 
+  suppressMessages(
+    ddd %>% left_join(dd %>% select(fr,to,dir)) %>% mutate(dir=dir*lag(dir)) %>% 
+    filter(dir %in% c(0,1)) %>% # if X is not on the same side of both sensors, the intervals move in different directions and we need to be careful before jumping
+    summarise(min(abs(gap))) %>%
+    unlist %>% unname()->ovl
+    ) # smallest overlap that isn't stable
+ 
+  # jump carefully... 
+  move<-min(d1$dX,   # ... not to byPass a sensor
+            ovl%/%2) # ... and to let overlaps vanish
   X<-X+max(1,move-1)
 }
 
@@ -94,7 +87,7 @@ X*bSup+l
 #13350458933732
 
 
-
+######################################################################
 
 # First strategy, ~brute force ----
 v<-0:bSup
